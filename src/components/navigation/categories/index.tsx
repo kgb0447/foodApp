f0f7a2670e418c19745f302dc645810a4102c6e2
Bennick Garcia <kaelgarciabennick1997@gmail.com>
2023-01-31 22:59:41 +0800

    feat: (31012023)optimized api and codes

--- src/components/navigation/categories/index.tsx
+++ src/components/navigation/categories/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import useFetch from '../../../utils/hooks/useFetch';
 import styles from './styles.module.scss'
+import APPCATEGORY from '../../../constants/cetegories';
 export default function Categories() {
   const {data} = useFetch('http://localhost:8888/menu');
+  
 
- 
+  if(!data){
+    return <div> .... loading</div>
+  }
+
+  const res =data.map((item:any)=> item.menuList);
+  const prices =  res.map((item:any)=> item.map((val:any)=> val));
+
+  const lowestPrice = prices.sort((a:any,b:any)=> a.price - b.price).slice(0,2);
+  console.log("lowestPrice:",lowestPrice)
+
   return (
     <div className={styles.categories}>
-      <div className={styles.categoryItems}>
-        <img src={require('../../../assets/img/logo.png')} alt="" />
-        <p>1</p>
-      </div>
-      <div className={styles.categoryItems}>2</div>
-      <div className={styles.categoryItems}>3</div>
-      <div className={styles.categoryItems}>4</div>
+      {
+        APPCATEGORY.map((item:any,index:number) => (
+          <div className={styles.categoryItems} key={item+index}>
+            <img src={require('../../../assets/img/logo.png')} alt="" />
+            <p>{item.label}</p>
+          </div>
+        ))
+      }
+      
+  
     </div>
   )
 }
---
+++ src/constants/cetegories.ts
@@ -0,0 +1,24 @@
+interface APPCATEGORY_TYPES{
+
+}
+
+const APPCATEGORY = [
+    {
+        code: 0,
+        label: 'Favorite'
+    },
+    {
+        code: 1,
+        label: 'Cheap'
+    },
+    {
+        code: 2,
+        label: 'Trend'
+    },
+    {
+        code: 3,
+        label: 'More'
+    }
+]
+
+export default APPCATEGORY;
